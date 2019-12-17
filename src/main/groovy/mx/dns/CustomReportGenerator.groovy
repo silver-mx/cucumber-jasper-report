fe227d8c77b65a58c7c102aada09d4406b8e0ae5
Diego Núñez Silva <diego.nunez.mx@gmail.com>
2019-12-17 11:27:56 +0100

    Improve report

--- src/main/groovy/mx/dns/CustomReportGenerator.groovy
+++ src/main/groovy/mx/dns/CustomReportGenerator.groovy
@@ -5,74 +5,66 @@ import groovy.json.JsonSlurper
 
 public class CustomReportGenerator {
 
-	File cucumberReport
+  File cucumberReport
 
-	public CustomReportGenerator(String cucumberReportPath) {
-		cucumberReport = new File(cucumberReportPath)
-		if (!cucumberReport.exists()) {
-			throw new IllegalArgumentException("The cucumber JSON report[$cucumberReportPath] does not exist.")
-		}
-	}
+  public CustomReportGenerator(String cucumberReportPath) {
+    cucumberReport = new File(cucumberReportPath)
+    if (!cucumberReport.exists()) {
+      throw new IllegalArgumentException("The cucumber JSON report[$cucumberReportPath] does not exist.")
+    }
+  }
 
-	public File generate() {
-		def report = new CustomReport()
+  public File generate() {
+    def scenarioLst = getScenariosAsJson()
+    int total = scenarioLst.size()
+    int failed = scenarioLst.count { it.result == "failed" }
+    int passed = total - failed
 
-		def scenarioLst = getScenariosAsJson()
-		int total = scenarioLst.size()
-		int failed = scenarioLst.count { it.result == "failed" }
-		int passed = total - failed
+    def builder = new JsonBuilder()
 
-		def builder = new JsonBuilder()
+    builder.report {
+      appName 'TBD'
+      version 'TBD'
+      environment 'TBD'
+      totalTests total
+      passedTests passed
+      failedTests failed
+      scenarios scenarioLst
+    }
 
-		builder.report {
-			totalTests total
-			testsPassed passed
-			testsFailed failed
-			scenarios scenarioLst
-		}
+    println builder.toPrettyString()
 
-		println builder.toPrettyString()
+    def customReportPath = cucumberReport.getParent() + "/customCucumberReport.json"
+    def customReport = new File(customReportPath)
+    customReport.text = builder.toPrettyString()
 
-		def customReportPath = cucumberReport.getParent() + "/customCucumberReport.json"
-		def customReport = new File(customReportPath)
-		customReport.text = builder.toPrettyString()
+    return customReport
+  }
 
-		return customReport
-	}
+  private List getScenariosAsJson() {
+    def cucumberData = new JsonSlurper().parse(cucumberReport)
+    def scenarios = []
 
-	private List getScenariosAsJson() {
-		def cucumberData = new JsonSlurper().parse(cucumberReport)
-		def scenarios = []
+    cucumberData.each { feature ->
+      def featureName = feature.name
 
-		cucumberData.each { feature ->
-			def featureName = feature.name
+      feature.elements.each { scenario ->
+        scenario.featureName = featureName
 
-			feature.elements.each { scenario ->
-				scenario.featureName = featureName
-				
-				def isFailed = scenario.steps.any { step -> step.result.status != "passed" }
-				
-				scenario.result = isFailed ? "failed" : "passed"
-				scenario.requirement = "NA"
-				scenarios.add(scenario)
-			}
-		}
+        def isFailed = scenario.steps.any { step -> step.result.status != "passed" }
 
-		return scenarios
-	}
+        scenario.result = isFailed ? "failed" : "passed"
+        scenario.requirement = "NA"
+        scenarios.add(scenario)
+      }
+    }
+
+    return scenarios
+  }
 
 
-	public static void main(String[] args) {
-		def gen = new CustomReportGenerator("/home/diego/dev/wa/cucumber-jasper-report/src/main/resources/cucumber.json")
-		assert gen.generate().exists() == true
-		/*<![CDATA[((net.sf.jasperreports.engine.data.JsonDataSource)$P{REPORT_DATA_SOURCE}).subDataSource("persons")]]>*/
-	}
-
-	private static class CustomReport {
-
-		Integer totalTests
-		Integer totalTestsPassed
-		Integer totalTestsFailed
-		List scenarios
-	}
+  public static void main(String[] args) {
+    def gen = new CustomReportGenerator("/home/diego/wa/cucumber-jasper-report/src/main/resources/cucumber.json")
+    assert gen.generate().exists() == true
+  }
 }
